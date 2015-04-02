b597c689359e02298bf934c43b2ab819c7f79533
Anne <star7anne@yahoo.com>
2015-04-02 13:38:20 -0700

    Initial Commit

---
+++ FacebookCodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacebookCodingChallenge
+//
+//  Created by Annemarie Ketola on 4/2/15.
+//  Copyright (c) 2015 UpEarly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacebookCodingChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FacebookCodingChallenge
+//
+//  Created by Annemarie Ketola on 4/2/15.
+//  Copyright (c) 2015 UpEarly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
